8efff9f71f48aa3c890b9330690a71b7210c65d4
dmitrybbch <d.babich@yahoo.com>
2018-05-30 18:42:05 +0200

    First Commit

---
+++ server.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>  //non ricordo cosa conteneva
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "C-Thread-Pool-master/thpool.h"
+#include <pthread.h>
+
+#define PORT "10000"       //porta del server
+
+void thMasterFunction(pthread_t *thread, pthread_attr_t *attr, void *(*start_routine)(void *)){
+
+}
+
+int main(int argc, char **argv){
+    pid_t pchild = fork();      //dubito di dover fare il fork subito subito, ripensaci
+    if(pchild == 0){
+        //codice del figlio
+        int masterSocket = socket(AF_INET, SOCK_STREAM, 0);
+    }
+    else{
+        //codice del padre
+    }
+    exit(0);
+}
