8168e308fa4aaf3ed5a8e864112c0f3215934373
dmitrybbch <d.babich@yahoo.com>
2018-06-03 18:48:08 +0200

    Iniziato l'inizializzazione

--- server.c
+++ server.c
@@ -63,7 +63,19 @@ int main(int argc, char *argv[]){
     socklen_t clilen; // Size of address
     struct sockaddr_in serv_addr, cli_addr; // Addresses
 
-    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
 
     return (EXIT_SUCCESS);
 }
